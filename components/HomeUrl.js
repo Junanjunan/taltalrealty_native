a236294b9cc614f10a748f053eaf5365e581068f
Taltal <taltalrealty@gmail.com>
2022-08-07 12:52:56 +0900

    .

--- components/HomeUrl.js
+++ components/HomeUrl.js
@@ -1,4 +1,4 @@
 const HomeUrl = "http://taltalrealty31-dev.ap-northeast-2.elasticbeanstalk.com";
-// const HomeUrl = "https://0a9f-121-130-224-115.jp.ngrok.io";
+// const HomeUrl = "https://038f-121-130-224-115.jp.ngrok.io";
 
 export default HomeUrl;
